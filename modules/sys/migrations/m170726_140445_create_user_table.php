fad90eed4d65d58dd99812b1dcd1d19ff34d75b9
Denis Utkin <dizirator@gmail.com>
2017-07-26 15:08:49 +0100

    Added user migration

---
+++ modules/sys/migrations/m170726_140445_create_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `user`.
+ */
+class m170726_140445_create_user_table extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8mb4 ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%user}}', [
+            'id'                   => $this->primaryKey(),
+            'username'             => $this->string(),
+            'auth_key'             => $this->string(32)->notNull(),
+            'password_hash'        => $this->string(),
+            'password_reset_token' => $this->string()->unique(),
+            'email'                => $this->string(),
+            'email_confirm_token'  => $this->string()->unique(),
+            'status'               => $this->smallInteger()->notNull()->defaultValue(0),
+            'created_at'           => $this->integer()->unsigned()->notNull(),
+            'updated_at'           => $this->integer()->unsigned()->notNull(),
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
