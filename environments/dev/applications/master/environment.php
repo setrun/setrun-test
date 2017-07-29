4b79169d4ffa3881bb4bf6a35e6abd133ab70e1b
Denis Utkin <dizirator@gmail.com>
2017-07-29 10:17:03 +0100

    Added fix to .gitignore

---
+++ environments/dev/applications/master/environment.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @author Denis Utkin <dizirator@gmail.com>
+ * @link   https://github.com/dizirator
+ */
+
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV')   or define('YII_ENV',   'dev');
---
+++ environments/prod/applications/master/environment.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @author Denis Utkin <dizirator@gmail.com>
+ * @link   https://github.com/dizirator
+ */
+
+defined('YII_DEBUG') or define('YII_DEBUG', false);
+defined('YII_ENV')   or define('YII_ENV',   'prod');
